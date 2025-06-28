454a9d19bc26d56690100f81f8029a5df7f7b254
Subratp12 <subrat.padhi1204@outlook.com>
2025-06-28 16:16:18 +0530

    Update backend-task-sevice.tf

--- ecs-task/backend-task-sevice.tf
+++ ecs-task/backend-task-sevice.tf
@@ -41,7 +41,7 @@ resource "aws_ecs_task_definition" "back-task" {
   container_definitions = jsonencode([
     {
       name      = "backend"
-      image     = "545009827818.dkr.ecr.us-east-1.amazonaws.com/backend:latest"
+      image     = "016830378686.dkr.ecr.ap-south-1.amazonaws.com/backend:latest"
       cpu       = 256
       memory    = 512
       essential = true
